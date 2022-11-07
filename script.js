d0cdeeec7b12b6d765c73965cf65c4b648a2659d
Alexishdjn <hadjianalexis@gmail.com>
2022-11-07 22:20:19 +0100

    Fix event links

--- script.js
+++ script.js
@@ -16,12 +16,10 @@ jQuery(document).ready(function($){
     
     });
 
-
-
     /*--------------------------------------------------------------
     Switch dark / light mode
     --------------------------------------------------------------*/
-
+    
     $(".site-header__left__theme-switcher").on('click', function() {
 
         if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
@@ -40,5 +38,52 @@ jQuery(document).ready(function($){
 
         }
     });
+
+
+
+    // Detect default color scheme
+
+
+
+
+    //Detect change
+
+    // window.matchMedia('(prefers-color-scheme: dark)').on('change', function() {
+        
+    //     if () {
+
+    //     }
+
+    // });
+
+
+    // $(".site-header__left__theme-switcher").on('click', function() {
+
+    //     if ( $("html").hasClass("os-default") ) {
+
+    //         if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
+
+    //             $("html").removeClass("os-default");
+    //             $("html").addClass("dark");
+    
+    //         } else if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
+                
+    //             $("html").removeClass("os-default");
+    //             $("html").addClass("light");
+    
+    //         }
+
+    //     } else if ( $("html").hasClass("light") ) {
+            
+    //         $("html").removeClass("light");
+    //         $("html").addClass("dark");
+
+    //     } else if ( $("html").hasClass("dark") ) {
+            
+    //         $("html").removeClass("dark");
+    //         $("html").addClass("light");
+
+    //     }
+    // });
 
 });
