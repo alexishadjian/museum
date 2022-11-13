709fc69b4e57defdd97e0fe47c4e3665926f68f1
Alexishdjn <hadjianalexis@gmail.com>
2022-11-13 21:22:50 +0100

    Dark mode system

--- script.js
+++ script.js
@@ -19,71 +19,128 @@ jQuery(document).ready(function($){
     /*--------------------------------------------------------------
     Switch dark / light mode
     --------------------------------------------------------------*/
-    
-    $(".site-header__left__theme-switcher").on('click', function() {
 
-        if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
-
-            $("html").removeClass("light");
-            $("html").toggleClass("dark");
-            $(".indicator").removeClass("light");
-            $(".indicator").toggleClass("dark");
-
-        } else if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
-            
-            $("html").removeClass("dark");
-            $("html").toggleClass("light");
-            $(".indicator").removeClass("dark");
-            $(".indicator").toggleClass("light");
-
-        }
-    });
+    //switchState c'est la position du switcher
+    //colorScheme c'est le theme de l'utilisateur
 
 
 
     // Detect default color scheme
+    if( localStorage.getItem('colorScheme') ) {
+        var switchState = localStorage.getItem('colorScheme');
+   	}
+
+
+    switchTheme();
+
+    // Detect change
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+        var colorScheme = event.matches ? "dark" : "light";
+
+        if ( colorScheme === "dark" ) {
+            switchState = "dark";
+        } else {
+            switchState = "light";
+        }
+
+        switchTheme();
+    });
+
+
+    // Switch
+	$('.site-header__left__theme-switcher').on('click', function() {
+		if( switchState === 'dark' ) {
+			switchState = 'light';
+		} else {
+			switchState = 'dark';
+		}
+
+		switchTheme();
+	});
+
+
+    // Function
+    function switchTheme() {
+		if ( switchState == 'dark' ) {
+			localStorage.setItem('colorScheme', 'dark');
+			$('html').addClass('dark').removeClass('light');
+			$('.indicator').addClass('dark').removeClass('light');
+			
+		} else if ( switchState == 'light' ) {
+			localStorage.setItem('colorScheme', 'light');
+			$('html').addClass('light').removeClass('dark');
+			$('.indicator').addClass('light').removeClass('dark');
+			
+		}
+	}
 
 
 
 
-    //Detect change
 
-    // window.matchMedia('(prefers-color-scheme: dark)').on('change', function() {
-        
-    //     if () {
 
+
+
+
+
+    //Prototype
+
+    // $(".site-header__left__theme-switcher").on('click', function()  {
+
+    //     if ( colorScheme == 'light' ) {
+    //         localStorage.setItem('colorScheme', 'dark');
+    //         $('html').addClass('dark').removeClass('light');
+    //         $('.indicator').addClass('dark').removeClass('light');
+            
+    //     } else if ( colorScheme == 'dark' ) {
+    //         localStorage.setItem('colorScheme', 'light');
+    //         $('html').addClass('light').removeClass('dark');
+    //         $('.indicator').addClass('light').removeClass('dark');
+            
     //     }
 
     // });
 
+    // $(".site-header__left__theme-switcher").on('click', function()  {
 
+    //     if ( $('html').hasClass('light') ) {
+
+    //         localStorage.setItem('currentScheme', 'dark');
+    //         $('html').addClass('dark').removeClass('light');
+    //         $('.indicator').addClass('dark').removeClass('light');
+    
+    //     } else if ( $('html').hasClass('dark') ) {
+    
+    //         localStorage.setItem('currentScheme', 'light');
+    //         $('html').addClass('light').removeClass('dark');
+    //         $('.indicator').addClass('light').removeClass('dark');
+    
+    //     }
+
+    // });
+
+    
+
+
+//Tout fonctionne suaf au reload
     // $(".site-header__left__theme-switcher").on('click', function() {
 
-    //     if ( $("html").hasClass("os-default") ) {
+    //     if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
 
-    //         if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
-
-    //             $("html").removeClass("os-default");
-    //             $("html").addClass("dark");
-    
-    //         } else if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
-                
-    //             $("html").removeClass("os-default");
-    //             $("html").addClass("light");
-    
-    //         }
-
-    //     } else if ( $("html").hasClass("light") ) {
-            
     //         $("html").removeClass("light");
-    //         $("html").addClass("dark");
+    //         $("html").toggleClass("dark");
+    //         $(".indicator").removeClass("light");
+    //         $(".indicator").toggleClass("dark");
 
-    //     } else if ( $("html").hasClass("dark") ) {
+    //     } else if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
             
     //         $("html").removeClass("dark");
-    //         $("html").addClass("light");
+    //         $("html").toggleClass("light");
+    //         $(".indicator").removeClass("dark");
+    //         $(".indicator").toggleClass("light");
 
     //     }
     // });
+
 
 });
